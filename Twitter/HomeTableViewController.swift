db52386a6b2afede0b242b54e1654a51bae736ac
anoushkaerram <anoushkaerram@gmail.com>
2021-03-01 21:36:06 -0800

    part 2 second commit -> finished retweet button

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -115,6 +115,8 @@ override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewC
         
         cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         
         return cell
     }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -12,6 +12,7 @@ class TweetCellTableViewCell: UITableViewCell {
     
     var favorited: Bool = false
     var tweetId: Int = -1
+    //var retweeted: Bool = false
     
 
     @IBOutlet weak var profileImageView: UIImageView!
@@ -46,7 +47,26 @@ class TweetCellTableViewCell: UITableViewCell {
         
     }
     
+    func setRetweeted(_ isRetweeted: Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+        
+    }
+    
     @IBAction func retweet(_ sender: Any) {
+        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting: \(error)")
+        })
+        
     }
     
     
